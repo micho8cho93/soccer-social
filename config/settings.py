f93209100e766070c88c3c2222ca0fcceb51bdff
micho8cho93 <182514846+micho8cho93@users.noreply.github.com>
2025-07-04 08:55:21 +0200

    updated config file to allow railway host to connect

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['soccersocial.up.railway.app', 'localhost', '127.0.0.1']
+
+CSRF_TRUSTED_ORIGINS = ['https://soccersocial.up.railway.app']
 
 
 # Application definition
