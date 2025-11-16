71553a6f11cdce1186cc64580a68e734e2ceae9e
micho8cho93 <182514846+micho8cho93@users.noreply.github.com>
2025-11-16 13:20:36 +0100

    fully integrated new landing page

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from futbolapp.urls import public_urlpatterns
-from futbolapp.views import landing_page
+from futbolapp.views import landing_page, referee_login, referee_portal, referee_match_update
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,5 +11,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('', landing_page, name='landing_page'),
-
+    path('referee_login/', referee_login, name='referee_login'),
+    path('referee_portal/', referee_portal, name='referee_portal'),
+    path('referee/match/<int:match_id>/update/', referee_match_update, name='referee_match_update'),
 ]
